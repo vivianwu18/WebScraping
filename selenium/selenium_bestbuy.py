3ed793b160ce6da29d27c32695596b8479312183
Meng-Wei Wu <mengweiwu18@gmail.com>
2023-03-01 21:41:32 -0800

    selenium bestbuy

--- selenium/selenium_bestbuy.py
+++ selenium/selenium_bestbuy.py
@@ -10,7 +10,7 @@ from selenium.webdriver.support import expected_conditions as EC
 ### Selenium
 ### 2. write a script that goes to bestbuy.com, clicks on Deal of the Day, reads how much time is left for the Deal of the Day and prints the remaining time to screen (console), clicks on the Deal of the Day (the actual product), clicks on its reviews, and saves the resulting HTML to your local hard drive as "bestbuy_deal_of_the_day.htm"
 
-### print the remaining time
+# print the remaining time
 driver = webdriver.Chrome(executable_path = '/Users/vivianwu/Desktop/BAX 422/chromedriver/chromedriver')
 driver.implicitly_wait(5)
 driver.set_script_timeout(120)
@@ -22,7 +22,7 @@ dealoftheday = driver.find_element(By.CSS_SELECTOR, "a[data-lid = 'hdr_dotd']")
 dealoftheday.click()
 time.sleep(1)
 
-### wait until the page finished loading
+# wait until the page finished loading
 WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "span[class = 'seconds cdnumber']")))
 hours = driver.find_element(By.CSS_SELECTOR, "span[class = 'hours cdnumber']").text
 minutes = driver.find_element(By.CSS_SELECTOR, "span[class = 'minutes cdnumber']").text
@@ -33,7 +33,7 @@ print(countdown)
 
 driver.quit()
 
-### save the reviews
+# save the reviews
 driver = webdriver.Chrome(executable_path = '/Users/vivianwu/Desktop/BAX 422/chromedriver/chromedriver')
 driver.implicitly_wait(5)
 driver.set_script_timeout(120)
